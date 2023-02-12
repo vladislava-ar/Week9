5f9ce29076dfe0d29b0afa4818cce45d25c24528
vladislava-ar <vladislava.dnk@gmail.com>
2023-02-12 22:33:24 +0200

    add project

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log(40000 + 22000);
+console.log("Владивосток" + " " + 2000);
+console.log(128 + 254 + 137 + 201);
+console.log(2 ** 5);
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!");
+
+const greetings = "Привет";
+const name = "Кот";
+alert(`${greetings}, ${name}`);
+
+let time; 
+time = 34;
+console.log(`Старт поездки. Осталось минут: ${time}`);
+time -= 15;
+console.log(`Немного сторис в соцсетях. Осталось минут: ${time}`);
+time -= 10; 
+console.log(`Немного не новостей, но событий. Осталось минут: ${time}`);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту — это ${celsius} градуса по Цельсию.`);
